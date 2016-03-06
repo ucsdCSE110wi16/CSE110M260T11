404890f811bac436556b3d0d94f9b9653c5a26b6
Nikkie Ella <Nikkie@Nikkies-Air.home>
2016-03-06 01:33:16 -0800

    Added a starting splash screen!

---
+++ Crowded/app/src/main/java/team11/crowded/splashScreen.java
@@ -0,0 +1,32 @@
+package team11.crowded;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                    Intent startMainScreen = new Intent(getApplicationContext(), login_screen.class);
+                    startActivity(startMainScreen);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
