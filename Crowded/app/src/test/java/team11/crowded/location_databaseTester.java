e44f21358cb9f6fc11ec50cc41b4ee7606c37533
daletubat <daletubat@yahoo.com>
2016-03-08 21:58:51 -0800

    skeleton for location_databaseTester

---
+++ Crowded/app/src/test/java/team11/crowded/location_databaseTester.java
@@ -0,0 +1,9 @@
+package team11.crowded;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class location_databaseTester.java{
+        private location_database test = new location_database();
+        }
